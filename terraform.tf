9465b92522ae06e9f0269284ea3c82789ea95746
Merl <phannakon.p@kkumail.com>
2024-09-19 00:41:31 +0700

    Fixed terraform

--- terraform.tf
+++ terraform.tf
@@ -46,9 +46,9 @@ spec:
             value: "true"
         - name: EUREKA_CLIENT_SERVICEURL_DEFAULTZONE
             value: "http://${var.EUREKA_URL}"
-        - name: "SECURITY_JWT_SECRET_KEY"
+        - name: SECURITY_JWT_SECRET_KEY
             value: ${var.JWT_SECRET}
-        - name: "SECURITY_JWT_EXPIRATION_TIME"
+        - name: SECURITY_JWT_EXPIRATION_TIME
             value: ${var.JWT_EXPIRATION_TIME}
       ports:
         - containerPort: 8080
